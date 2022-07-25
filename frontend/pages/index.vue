5259ae3186faccd35ad44c0f4da6e9337b237c6f
avimehenwal <avi.mehanwal@gmail.com>
2022-07-25 20:26:49 +0200

    feat(nuxt): :zap: use vuetify with nuxt3
    https://github.com/nuxt/framework/discussions/1183

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <h1>Index page</h1>
+  <div class="d-flex align-center flex-column">
+    <v-icon>mdi-home</v-icon>
+    <v-card width="400">
+      <v-card-header>
+        <v-card-header-text>
+          <v-card-title>This is a title</v-card-title>
+          <v-card-subtitle>This is a subtitle</v-card-subtitle>
+        </v-card-header-text>
+      </v-card-header>
+
+      <v-card-text> This is content </v-card-text>
+    </v-card>
+  </div>
 </template>
